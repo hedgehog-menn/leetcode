e29d67e9f1ab94364dcf39662072382accf13a42
KT <kp.thananchai@gmail.com>
2025-02-03 16:23:22 +0800

    Time: 5 ms (25.32%) | Memory: 53 MB (65.46%) - LeetSync

--- top-interview-150/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ top-interview-150/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,4 +1,5 @@
 function removeDuplicates(nums: number[]): number {
+    /*
     let i = 0;
     while (i < nums.length) {
         if (nums[i] === nums[i + 1]) {
@@ -7,5 +8,16 @@ function removeDuplicates(nums: number[]): number {
             i++;
         }
     }
+    */
+
+    const noDupArr = [];
+    nums.forEach(item => {
+        if (!noDupArr.includes(item)) {
+            noDupArr.push(item);
+        }
+    });
+    nums.splice(0, nums.length);
+    nums.push(...noDupArr);
+
     return nums.length;
 };
