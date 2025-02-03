966925b8000b9d1da1da655a7a9e44e88d99101a
KT <kp.thananchai@gmail.com>
2025-02-03 14:57:02 +0800

    Time: 59 ms (8.83%) | Memory: 54.1 MB (13.46%) - LeetSync

---
+++ top-interview-150/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    return nums.length;
+};
