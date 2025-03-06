281e4ced177a111d5e4a5a4cf94caf478f75e58b
KT <kp.thananchai@gmail.com>
2025-03-06 15:10:02 +0800

    Time: 0 ms (100.00%) | Memory: 67.4 MB (8.26%) - LeetSync

--- top-interview-150/189-rotate-array/rotate-array.ts
+++ top-interview-150/189-rotate-array/rotate-array.ts
@@ -4,10 +4,12 @@
 function rotate(nums: number[], k: number): void {
     if (k == 0)
         return;
-    
+
     // handle k larger that nums size
     const lastK = k % nums.length;
 
-    const tail = nums.splice(-lastK);
-    nums.unshift(...tail);
+    // const tail = nums.splice(-lastK);
+    // nums.unshift(...tail);
+
+    nums.unshift(...nums.splice(-lastK));
 };
