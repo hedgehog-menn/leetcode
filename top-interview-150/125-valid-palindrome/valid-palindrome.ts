d7e02ca9e3b35694f06160ca52ae88b3efde3011
KT <kp.thananchai@gmail.com>
2025-03-08 17:13:01 +0800

    Time: 4 ms (75.57%) | Memory: 58.1 MB (31.96%) - LeetSync

--- top-interview-150/125-valid-palindrome/valid-palindrome.ts
+++ top-interview-150/125-valid-palindrome/valid-palindrome.ts
@@ -1,4 +1,14 @@
 function isPalindrome(s: string): boolean {
     const str =  s.toLowerCase().replace(/[^a-z0-9]+/g, '');
-    return str == str.split('').reverse().join('');
+    // return str == str.split('').reverse().join('');
+
+    const len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+
+        // Comparing i th character from starting
+        //  and len-i th character from end
+        if (str[i] !== str[len - i - 1])
+            return false;
+    }
+    return true;
 };
