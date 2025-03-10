11bac4afa071538d449f560c2b2adc0f894ee26d
KT <kp.thananchai@gmail.com>
2025-03-10 23:09:03 +0800

    Time: 2 ms (77.58%) | Memory: 65.8 MB (15.43%) - LeetSync

--- top-interview-150/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ top-interview-150/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -3,16 +3,15 @@ function maxProfit(prices: number[]): number {
     let maxProfit = 0;
 
     for (let i = 1; i < prices.length; i++) {
-        /*
         if (prices[i] < minPrice) {
             minPrice = prices[i];
         }
         if (prices[i] - minPrice > maxProfit) {
             maxProfit = prices[i] - minPrice;
         }
-        */
-        minPrice = Math.min(minPrice, prices[i]);
-        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    
+        // minPrice = Math.min(minPrice, prices[i]);
+        // maxProfit = Math.max(maxProfit, prices[i] - minPrice);
     }
 
     return maxProfit;
