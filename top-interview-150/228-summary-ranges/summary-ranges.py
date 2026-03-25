37e992c7761a8b896e202db53f0c78557edfe70e
KT <kp.thananchai@gmail.com>
2026-03-26 02:20:03 +0800

    Time: 0 ms (100.00%) | Memory: 12.3 MB (95.74%) - LeetSync

--- top-interview-150/228-summary-ranges/summary-ranges.py
+++ top-interview-150/228-summary-ranges/summary-ranges.py
@@ -1,25 +1,24 @@
-class Solution:
-    def summaryRanges(self, nums: List[int]) -> List[str]:
-        if len(nums) == 0:
-            return []
-
-        first_of_range = nums[0]
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        i = 0
         output = []
-        for i in range(len(nums) - 1):
-            if nums[i] + 1 == nums[i+1]:
-                if i == len(nums) - 2:
-                    output.append(str(first_of_range) + "->" + str(nums[-1]))
-            elif first_of_range == nums[i]:
-                output.append(str(nums[i]))
-                first_of_range = nums[i+1]
-            else:
-                output.append(str(first_of_range) + "->" + str(nums[i]))
-                first_of_range = nums[i+1]
-
-        if first_of_range == nums[-1]:
-            output.append(str(nums[-1]))
+        while i < len(nums):
+            j = i
+            while j < len(nums) - 1 and nums[j] + 1 == nums[j + 1]:
+                j += 1
             
+            if  nums[i] == nums[j]:
+                output.append(str(nums[i]))
+            else:
+                output.append("{}->{}".format(nums[i], nums[j]))
+            
+            i = j + 1
 
         return output
-            
+                
+                
         
