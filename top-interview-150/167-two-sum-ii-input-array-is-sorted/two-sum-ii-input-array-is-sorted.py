808adedbea266f3477b59cad97708ebfbb0e04e7
KT <kp.thananchai@gmail.com>
2026-03-27 21:43:12 +0800

    Time: 7 ms (28.91%) | Memory: 20.6 MB (59.95%) - LeetSync

--- top-interview-150/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ top-interview-150/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,28 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
 
-        """
-        # correct, but still too long
-        for i in range(len(numbers)):
-            j = i + 1
-
-            while j < len(numbers):
-                if numbers[i] + numbers[j] == target:
-                    return [i+1, j+1]
-                elif numbers[i] + numbers[j] > target:
-                    break
-                else:
-                    j += 1
-        """
-
-        i = 0
-        j = len(numbers) - 1
-
-        while i < j:
-            sum = numbers[i] + numbers[j]
-            if sum == target:
-                return [i+1, j+1]
-            if sum < target:
-                i += 1
-            if sum > target:
-                j -= 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
