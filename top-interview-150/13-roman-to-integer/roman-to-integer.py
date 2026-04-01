9148d4abae78bc4cf67b483242913137085fb5ce
KT <kp.thananchai@gmail.com>
2026-04-01 19:01:18 +0800

    Time: 7 ms (42.27%) | Memory: 19.5 MB (12.34%) - LeetSync

--- top-interview-150/13-roman-to-integer/roman-to-integer.py
+++ top-interview-150/13-roman-to-integer/roman-to-integer.py
@@ -4,27 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman = s
+        roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
         result = 0
-        if "IV" in roman:
-            result += 4
-            roman = roman.replace("IV", "")
-        if "IX" in roman:
-            result += 9
-            roman = roman.replace("IX", "")
-        if "XL" in roman:
-            result += 40
-            roman = roman.replace("XL", "")
-        if "XC" in roman:
-            result += 90
-            roman = roman.replace("XC", "")
-        if "CD" in roman:
-            result += 400
-            roman = roman.replace("CD", "")
-        if "CM" in roman:
-            result += 900
-            roman = roman.replace("CM", "")
+        
+        for i in range(1, len(s)):
+            result = result - roman_dict[s[i - 1]] if roman_dict[s[i - 1]] < roman_dict[s[i]] else result + roman_dict[s[i - 1]]
 
-        result += roman.count("I") + roman.count("V")*5 + roman.count("X")*10 + roman.count("L")*50 + roman.count("C")*100 + roman.count("D")*500 + roman.count("M")*1000
+        result += roman_dict[s[len(s) -1]]
 
         return result
