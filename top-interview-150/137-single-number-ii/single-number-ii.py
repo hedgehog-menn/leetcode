0e615ab6347a4c6afea356fe710dd8c52ec5b2a1
KT <kp.thananchai@gmail.com>
2026-04-23 17:00:51 +0800

    Time: 1 ms (81.94%) | Memory: 20.6 MB (67.17%) - LeetSync

---
+++ top-interview-150/137-single-number-ii/single-number-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        nums.sort()
+
+        if nums[0] != nums[1]:
+            return nums[0]
+
+        for i in range(1, len(nums) - 1):
+            if nums[i - 1] != nums[i] != nums[i + 1]:
+                return nums[i]
+        
+        return nums[-1]
+        
