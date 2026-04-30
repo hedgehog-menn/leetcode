e6cbaaab6f0ef8e9b80632ef4723d3638d71d872
KT <kp.thananchai@gmail.com>
2026-04-30 21:01:23 +0800

    Time: 1 ms (29.90%) | Memory: 19.3 MB (86.39%) - LeetSync

--- top-interview-150/50-powx-n/powx-n.py
+++ top-interview-150/50-powx-n/powx-n.py
@@ -1,4 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        def fast_pow(x, n):
+            result = 1.0
+            while n:
+                if n % 2 == 1:
+                    result *= x
+                x *= x
+                n //= 2
+            return result
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        return fast_pow(x, n)
         
