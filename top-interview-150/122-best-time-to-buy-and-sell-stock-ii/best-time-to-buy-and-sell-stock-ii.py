a0c742ca148a736c1ac50fbf3e45fddafa956f08
KT <kp.thananchai@gmail.com>
2026-06-05 21:43:21 +0800

    Time: 0 ms (100.00%) | Memory: 20.3 MB (51.34%) - LeetSync

--- top-interview-150/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ top-interview-150/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,27 +1,7 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        # Sell imdiedately when profit == max profit
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        """
-        buyPrice = prices[0]
-        for currentPrice in prices:
-            if currentPrice < buyPrice:
-                buyPrice = currentPrice
-            else:
-                profit += currentPrice - buyPrice
-                buyPrice = currentPrice
-        """
-
         for i in range(1, len(prices)):
-            if prices[i - 1] < prices[i]:
+            if prices[i] > prices[i - 1]:
                 profit += prices[i] - prices[i - 1]
-
         return profit
-                
-
-            
-        
