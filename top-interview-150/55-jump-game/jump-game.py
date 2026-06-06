35b209da060f2d78bfcfcfd792ed65a4805a01c8
KT <kp.thananchai@gmail.com>
2026-06-06 23:33:51 +0800

    Time: 15 ms (80.00%) | Memory: 20.1 MB (97.87%) - LeetSync

--- top-interview-150/55-jump-game/jump-game.py
+++ top-interview-150/55-jump-game/jump-game.py
@@ -1,18 +1,7 @@
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
         goal = len(nums) - 1
-
-        for i in reversed(range(len(nums) - 1)):
+        for i in range(len(nums) - 1, -1, -1):
             if nums[i] + i >= goal:
                 goal = i
-
-        # able to be back at the first index
         return goal == 0
-
-        
-        
