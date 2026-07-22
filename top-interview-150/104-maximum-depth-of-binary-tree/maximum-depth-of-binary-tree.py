18ccc4f63a958f354a19ce6b20e39e026417b66e
KT <kp.thananchai@gmail.com>
2026-07-23 03:53:35 +0800

    Time: 6 ms (6.29%) | Memory: 23.3 MB (8.12%) - LeetSync

--- top-interview-150/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ top-interview-150/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,7 +6,16 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # Method 1: Recursive DFS
+        """
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        """
+
         # Method 2: BFS
+        """
         if not root:
             return 0
 
@@ -21,4 +30,19 @@ class Solution:
                     q.append(node.right)
             level += 1
         return level
+        """
+
+        # Method 3: Iterative DFS
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
         
