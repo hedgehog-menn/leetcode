9fcffab3eeaead9c6122652c036687b0dc2d51b5
KT <kp.thananchai@gmail.com>
2026-08-08 03:42:30 +0800

    Time: 1 ms (49.26%) | Memory: 19.2 MB (62.42%) - LeetSync

--- top-interview-150/71-simplify-path/simplify-path.py
+++ top-interview-150/71-simplify-path/simplify-path.py
@@ -2,17 +2,14 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
         current = ""
-
         for c in path + "/":
             if c == "/":
                 if current == "..":
-                    if stack: # if not empty
+                    if stack:
                         stack.pop()
                 elif current != "" and current != ".":
                     stack.append(current)
                 current = ""
             else:
                 current += c
-
         return "/" + "/".join(stack)
-        
